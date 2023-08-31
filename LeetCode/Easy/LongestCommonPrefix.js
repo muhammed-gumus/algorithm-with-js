aa8f6282eacfab2635628d7175fc440b2c6998a2
muhammed-gumus <mr.silver.mg@gmail.com>
2023-09-01 01:15:20 +0300

    feat(LeetCode/Easy): solve Longest Common Prefix algorithm

---
+++ LeetCode/Easy/LongestCommonPrefix.js
@@ -0,0 +1,40 @@
+// Longest Common Prefix
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+// Constraints:
+
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lowercase English letters.
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (!strs.length) return "";
+
+  for (let i = 0; i <= strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) {
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+
+  return strs[0];
+};
