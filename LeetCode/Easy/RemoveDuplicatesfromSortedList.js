5aebe57f155bf3d725511219570a604f14ba8e90
muhammed-gumus <mr.silver.mg@gmail.com>
2023-09-01 01:40:49 +0300

    feat(LeetCode/Easy): solve Remove Duplicates from Sorted List algorithm

---
+++ LeetCode/Easy/RemoveDuplicatesfromSortedList.js
@@ -0,0 +1,44 @@
+// Remove Duplicates from Sorted List
+
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+// Example 1:
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+
+// Example 2:
+
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 300].
+// -100 <= Node.val <= 100
+// The list is guaranteed to be sorted in ascending order.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  let current = head;
+
+  while (current) {
+    if (current.next && current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return head;
+};
