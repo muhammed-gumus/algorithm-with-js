3a0f9f1710ef0572c9e6bd0d6d375d85fdf38f5b
muhammed-gumus <mr.silver.mg@gmail.com>
2024-12-11 16:02:39 +0300

    added new algorithm problem

---
+++ LeetCode/Easy/FindtheIndexoftheFirstOccurrenceinaString.js
@@ -0,0 +1,49 @@
+// Find the Index of the First Occurrence in a String
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+// Constraints:
+
+// 1 <= haystack.length, needle.length <= 104
+// haystack and needle consist of only lowercase English characters.
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  if (needle === "") {
+    return 0;
+  }
+  if (haystack.length < needle.length) {
+    return -1;
+  }
+
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    let match = true;
+    for (let j = 0; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        match = false;
+        break;
+      }
+    }
+    if (match) {
+      return i;
+    }
+  }
+  return -1;
+};
